55a9cf664aa07b29a84244097901dd8256b5b246
Chuntao Hong <chuntao.hong@gmail.com>
2015-08-21 17:01:39 +0800

    initial commit

---
+++ DownloadFile/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+namespace DownloadFile
+{
+    class Program
+    {
+        static int numParallelDownload = 8;
+        static SortedDictionary<int, ProcessAndInfo> finishedProcesses = new SortedDictionary<int,ProcessAndInfo>();
+        static List<ProcessAndInfo> runningProcesses = new List<ProcessAndInfo>();
+
+        class ProcessAndInfo
+        {
+            public Process process;
+            public int idx;
+            public string cmd;
+            public StreamReader stdout;
+            public StreamReader stderr;
+        };
+        static void FinishOneProcess(ProcessAndInfo pinfo)
+        {
+            Console.WriteLine("****************************************");
+            Console.WriteLine(pinfo.idx + " : " + pinfo.process.ExitCode + " : " + pinfo.cmd);
+            Console.WriteLine("================= stdout ===============");
+            Console.WriteLine(pinfo.stdout.ReadToEnd());
+            Console.WriteLine("================= stderr ===============");
+            Console.Write(pinfo.stderr.ReadToEnd());
+            finishedProcesses.Add(pinfo.idx, pinfo);
+            runningProcesses.Remove(pinfo);
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 5)
+            {
+                Console.WriteLine("usage: DownloadFile prefix localDir numParts numWorkers workerId");
+                return;
+            }
+            string prefix = args[0];
+            string localDir = args[1];
+            int nTrainParts = int.Parse(args[2]);
+            int nParts = int.Parse(args[3]);
+            int partId = int.Parse(args[4]);
+
+            int partPerNode = Math.Max((nTrainParts + nParts - 1) / nParts, 1);
+            int partBegin = Math.Min(partPerNode * partId, nTrainParts);
+            int partEnd = Math.Min(partPerNode * (partId + 1), nTrainParts);
+
+            
+            for (int i = partBegin; i < partEnd; i++)
+            {
+                while (runningProcesses.Count >= numParallelDownload)
+                {
+                    var pr = runningProcesses.ElementAt(0);
+                    pr.process.WaitForExit();
+                    FinishOneProcess(pr);
+                }
+                string path = prefix + "/part-" + i.ToString("00000");
+                string cmd = "hdfs dfs -get " + path + " " + localDir + "\\";
+                Console.WriteLine(cmd);
+                var process = new System.Diagnostics.Process();
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.FileName = @"cmd.exe";
+                process.StartInfo.Arguments = "/c " + cmd;
+                process.Start();
+                ProcessAndInfo pinfo = new ProcessAndInfo();
+                pinfo.process = process;
+                pinfo.idx = i;
+                pinfo.cmd = cmd;
+                pinfo.stdout = process.StandardOutput;
+                pinfo.stderr = process.StandardError;
+                runningProcesses.Add(pinfo);
+            }
+            while (runningProcesses.Count > 0)
+            {
+                foreach (var pinfo in runningProcesses.ToArray())
+                {
+                    pinfo.process.WaitForExit();
+                    FinishOneProcess(pinfo);
+                }
+            }
+
+            Console.WriteLine("****************************************");
+            Console.Write("failures: ");
+            int failures = 0;
+            foreach (var kv in finishedProcesses)
+            {
+                int idx = kv.Key;
+                ProcessAndInfo pinfo = kv.Value;
+                if (pinfo.process.ExitCode != 0)
+                {
+                    failures++;
+                    Console.Write("\t" + idx);
+                }
+            }
+
+            System.Environment.Exit(failures);
+        }
+    }
+}
---
+++ RunOnAllMachines/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+using System.Configuration;
+
+namespace RunOnAllMachines
+{
+    class ProcessAndInfo
+    {
+        public string machineName;
+        public Process process;
+        public int machineId;
+        public StreamReader stdout;
+        public StreamReader stderr;
+    };
+
+    class Program
+    {
+        static int maxNumProcess = 20;
+        static string logDir = "Logs";
+
+        static SortedDictionary<int, string> allMachines = new SortedDictionary<int, string>();
+        static Dictionary<string, int> finishedProcesses = new Dictionary<string, int>();
+        static List<ProcessAndInfo> currentProcesses = new List<ProcessAndInfo>();
+
+        static void FinishOneProcess(ProcessAndInfo pinfo)
+        {
+            var f = new StreamWriter(logDir + "\\" + pinfo.machineName + ".txt");
+            f.WriteLine("================= stdout ===============");
+            f.WriteLine(pinfo.stdout.ReadToEnd());
+            f.WriteLine("================= stderr ===============");
+            f.Write(pinfo.stderr.ReadToEnd());
+            f.Close();
+
+            string m = pinfo.machineName;
+            int c = pinfo.process.ExitCode;
+            finishedProcesses.Add(m, c);
+            Console.WriteLine("[{0}/{1}] {2} finished with code {3}, active={4}", finishedProcesses.Count, allMachines.Count, m, c, currentProcesses.Count());
+            currentProcesses.Remove(pinfo);
+        }
+
+        static void Main(string[] args)
+        {
+            //ProcessStartInfo si = new ProcessStartInfo();
+            //si.FileName = @"cmd.exe";
+            //si.Arguments = @"/c robocopy d:\tmp \\srgssd-01\f$\chhong\tmp /MIR";
+            //var pr = Process.Start(si);
+            //while (!pr.HasExited)
+            //{
+            //    System.Threading.Thread.Sleep(1000);
+            //}
+            //return;
+
+
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: RunOnAllMachines machineList command [maxNumProcess] [logDir]");
+                return;
+            }
+            if (args.Length >= 3)
+                maxNumProcess = int.Parse(args[2]);
+            if (args.Length >= 4)
+                logDir = args[3];
+
+            if (!Directory.Exists(logDir))
+            {
+                Directory.CreateDirectory(logDir);
+            }
+
+            System.IO.StreamReader file = new System.IO.StreamReader(args[0]);
+            string line;
+            int counter = 0;
+            while ((line = file.ReadLine()) != null)
+            {
+                counter++;
+                var machine = line.Trim();
+                if (machine == "") continue;
+                machine = machine.Split()[0];
+                if (allMachines.ContainsValue(machine))
+                {
+                    Console.WriteLine("duplicate machine {0} on line {1}", machine, counter);
+                    continue;
+                }
+                allMachines.Add(allMachines.Count, machine);
+            }
+            file.Close();
+
+            foreach (var item in allMachines)
+            {
+                var index = item.Key;
+                var machine = item.Value;
+                while(currentProcesses.Count >= maxNumProcess)
+                {
+                    foreach (var pinfo in currentProcesses.ToArray())
+                    {
+                        pinfo.process.WaitForExit();
+                        if (pinfo.process.HasExited)
+                        {
+                            FinishOneProcess(pinfo);
+                        }
+                        if (currentProcesses.Count < maxNumProcess)
+                            break;
+                    }
+                }
+                
+                ProcessStartInfo start = new ProcessStartInfo();
+                start.FileName = @"cmd.exe";
+                start.Arguments = "/c " + args[1];
+                start.UseShellExecute = false;
+                start.RedirectStandardOutput = true;
+                start.RedirectStandardError = true;
+                start.EnvironmentVariables["_MachineCount_"] = allMachines.Count.ToString();
+                start.EnvironmentVariables["_MachineId_"] = index.ToString();
+                start.EnvironmentVariables["_MachineName_"] = machine;
+                ProcessAndInfo p = new ProcessAndInfo();
+                p.process = Process.Start(start);
+                p.machineId = index;
+                p.machineName = machine;
+                p.stdout = p.process.StandardOutput;
+                p.stderr = p.process.StandardError;
+                currentProcesses.Add(p);
+            }
+            while (currentProcesses.Count != 0)
+            {
+                foreach (var pinfo in currentProcesses.ToArray())
+                {
+                    pinfo.process.WaitForExit();
+                    if (pinfo.process.HasExited)
+                    {
+                        FinishOneProcess(pinfo);
+                    }
+                }
+            }
+            Console.WriteLine("failed machines: ");
+            foreach (var mc in finishedProcesses)
+            {
+                string m = mc.Key;
+                int c = mc.Value;
+                if (c != 0)
+                {
+                    Console.Write(m + "\t");
+                }
+            }
+        }
+
+    }
+}
